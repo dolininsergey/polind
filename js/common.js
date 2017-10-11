0fd10c827405b37d97512c5012009979e54e4840
Sergey <code.sergey.dolinin@gmail.com>
2017-10-11 22:33:51 +0300

    comleted.

--- js/common.js
+++ js/common.js
@@ -1,44 +1,36 @@
 $(function() {
 
-	$('.search-button').click(function() {
-		$('.search-form').animate({width: 'toggle'});
-	});
+		$('.search-button').click(function() {
+			$('.search-form').animate({width: 'toggle'});
+		});
 
 
-  $(".owl-carousel").owlCarousel({
-  	loop: false,
-  	//autoplay: true,
-  	//autoplayTimeout: 1000,
-  	items: 5,
-  	margin: 15,
-  	responsiveClass: true,
-  	responsive : {
-          0 : {
-              items: 1
-          },
-          768 : {
-              items: 3
-          },
-          960 : {
-              items: 4
-          },
-          1200 : {
-              items: 5
-          },
-      }
-  	//center: true,
-  });
+	  $(".owl-carousel").owlCarousel({
+	  	loop: true,
+	  	items: 5,
+	  	margin: 15,
+	  	responsiveClass: true,
+	  	responsive : {
+	          0 : {
+	              items: 1
+	          },
+	          768 : {
+	              items: 3
+	          },
+	          960 : {
+	              items: 4
+	          },
+	          1200 : {
+	              items: 5
+	          },
+	      }
+	  });
 
 
-    // $('.center').each(setInterval(function(){
-    //     //$(this).addClass("ben");
-    //     $(this).children().css("border", "solid 1px #fff");
-    // }, 1000));
-    // $("owl-item").each(function() {
-    // 	if($(this)).hasClass("center") {
-    // 		$(this).css("border", "solid 1px #fff");
-    // 	}
-    // });
-//setInterval (function(){}, 5000);
+		$(".oc-item").hover(function() {
+			$(this).find('.button').css('display', 'inline-block');
+		}, function() {
+			$(this).find('.button').css('display', 'none');
+		});
 
 });
